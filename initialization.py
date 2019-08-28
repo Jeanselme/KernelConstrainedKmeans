fb7af2b078b62a92b7d347b4da9085103e48b10e
Vincent <vincent.jeanselme@gmail.com>
2019-08-28 14:55:28 -0400

    Stop verifying constraints for initialization (sparse)

--- initialization.py
+++ initialization.py
@@ -35,18 +35,15 @@ class Initialization:
         self.constraint = constraint
         assert self.number >= k, "Constraint too important for number of cluster"
 
-        if (count > 1).sum() >= k:
-            print("Constraints do not allow to find enough connected components for farthest first")
+        if (count > 1).sum() < k:
+            print("Constraints do not allow to find enough connected components for farthest first ({} for {} classes) => Random forced".format((count > 1).sum(), k))
+            self.farthest_initialization = lambda x: None
 
         self.k = k
 
-    def _finalize_assignation(self, ):
-        # TODO: Clean assignation of the remaining connected components
-        return
-        
     def farthest_initialization(self, kernel):
         """
-            Farthest points that verify constraint
+            Farthest points that verifies constraint
 
             Arguments:
                 kernel {Array n * n} -- Kernel matrix (n * n)
@@ -101,11 +98,7 @@ class Initialization:
                 for c in match}
 
             ## Closest verifying constraint
-            order = sorted(distance.keys(), key = lambda k: distance[k])
-
-            ## If no constraint is positive => Too much constraint
-            broken_constraint = self.constraint[:, assignation_cluster[i].flatten()]
-            closest = min(order, key=lambda o: np.sum(broken_constraint[(assignations == o),:] < 0))
+            closest = min(distance.keys(), key = lambda k: distance[k])
             assignations[assignation_cluster[i].flatten()] = match[closest]
 
             ## Update assignation closest
@@ -115,20 +108,83 @@ class Initialization:
 
         return assignations
 
-    def kmeanspp_initialization(self, kernel):
-        """
-            Kmeans ++ that verify constraint
-
-            Arguments:
-                kernel {Array n * n} -- Kernel matrix (n * n)
-        """
-        # Compute the farthest centers
-        assignations = np.full_like(self.components, np.nan)
-
-        return assignations
-
-    def random_initialization(self, kernel = None):
+    def random_initialization(self):
         """
             Random Assignation
         """
         return np.random.choice(self.k, size = len(self.components))
+
+class Euclidean_Initialization(Initialization):
+
+    @classmethod
+    def compute_center(cls, data, assignation):
+        """
+            Computes euclidean centers given an assignation
+            
+            Arguments:
+                data {[type]} -- [description]
+                assignation {[type]} -- [description]
+        """
+        centers = []
+        for i in np.unique(assignation):
+            centers.append(data[assignation == i].mean(0))
+        return np.vstack(centers)
+
+    def farthest_initialization(self, data):
+        """
+            Farthest points that verifies constraint
+
+            Arguments:
+                data {Array n * d} -- Data
+        """
+        # Compute the farthest centers
+        assignations = np.full_like(self.components, np.nan)
+
+        # Precompute center distances
+        assignation_cluster, center_cluster, intra_number = {}, {}, {}
+        for c in range(self.number):
+            assignation_cluster[c] = (self.components == c).reshape((-1,1))
+            center_cluster[c] = data[self.components == c].mean(0)
+            intra_number[c] = assignation_cluster[c].sum()
+
+        ## First assignation is the largest component
+        assignations[self.components == 0] = 0
+
+        ## Save characteristics of assigned components
+        assigned_center = data[self.components == 0].mean(0)
+        remaining = set(range(1, self.number))
+
+        ## Compute iteratively the farthest given all other
+        match = {}
+        for i in range(1, self.k):
+            # Computes distances to all remaining NON TRIVIAL connected components
+            # We ignore the last part which depends on the intravariance of the past clusters
+            distance = {c: np.linalg.norm(assigned_center - center_cluster[c])
+                for c in remaining if intra_number[c] > 1}
+
+            farthest = max(distance, key = lambda x: distance[x])
+            assignations[assignation_cluster[farthest].flatten()] = i
+            match[farthest] = i
+
+            # Update characteristics of assigned
+            assigned_center = data[assignations >= 0].mean(0)
+
+            # Remove components
+            remaining.remove(farthest)
+
+        # Assign each unassigned components
+        for i in remaining:
+            ## Computes distances to all other cluster 
+            ## We ignore the last part which depends on the intravariance of the cluster i
+            distance = {c: np.linalg.norm(center_cluster[i] - center_cluster[c])
+                for c in match}
+
+            ## Closest verifying constraint
+            closest = min(distance.keys(), key = lambda k: distance[k])
+            assignations[assignation_cluster[i].flatten()] = match[closest]
+
+            ## Update assignation closest
+            assignation_cluster[closest] += assignation_cluster[i]
+            center_cluster[closest] = data[assignation_cluster[closest]].mean(0)
+
+        return assignations
